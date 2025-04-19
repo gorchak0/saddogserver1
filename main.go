d3772457f0348c2ddb68a2cfdec0f4f68d796385
winnettop <sorokinstepan@yandex.com>
2025-04-19 21:20:12 +0300

    123

--- main.go
+++ main.go
@@ -3,14 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os/exec"
 	"time"
 )
 
 func main() {
+	// Обработчик для главной страницы
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		currentTime := time.Now().Format("2006-01-02 15:04:05")
-		fmt.Fprintf(w, "Текущее время сервера: %s", currentTime)
+		fmt.Fprintf(w, "Текущее время сервера13: %s", currentTime)
 	})
 
+	// Обработчик для вебхука
+	http.HandleFunc("/update", func(w http.ResponseWriter, r *http.Request) {
+		// Запуск скрипта для обновления
+		cmd := exec.Command("cmd", "/C", "H:\\saddogserver1\\deploy.bat")
+		err := cmd.Run()
+		if err != nil {
+			http.Error(w, "Ошибка при выполнении скрипта обновления", http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintln(w, "Обновление выполнено успешно!")
+	})
+
+	// Запуск сервера
 	http.ListenAndServe(":8080", nil)
 }
