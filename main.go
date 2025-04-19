b2048e064ab6bc5d55b0f4e65934e1148186deb5
winnettop <sorokinstepan@yandex.com>
2025-04-19 17:09:47 +0300

    123

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		currentTime := time.Now().Format("2006-01-02 15:04:05")
+		fmt.Fprintf(w, "Текущее время сервера: %s", currentTime)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
